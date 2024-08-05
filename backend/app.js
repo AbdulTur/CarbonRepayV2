0f7b26fcf1f03da62b70d343e4a9cc7b34242cda
AbdulTur <aturonov@mun.ca>
2024-08-04 22:09:23 -0230

    Deploy to Heroku

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 const vehicleRoutes = require('./routes/vehicleRoutes');
 const distanceRoutes = require('./routes/distanceRoutes');
 const emissionRoutes = require('./routes/emissionRoutes');
@@ -12,12 +13,22 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes); // Ensure this is used correctly
 app.use('/api/vehicles', vehicleRoutes);
 app.use('/api/distances', distanceRoutes);
 app.use('/api/emissions', emissionRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 module.exports = app;
